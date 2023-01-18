96cfb89e37e886f8e670f043eddb3a5fed300516
jondoetsch27 <46655911+jondoetsch27@users.noreply.github.com>
2023-01-18 14:33:15 -0700

    project creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three'
+import * as React from 'react'
+import { Canvas } from '@react-three/fiber'
 
 function App() {
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xffcb05)
+  const perspectiveCamera = new THREE.PerspectiveCamera()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas camera={perspectiveCamera}>
+      <div />
+    </Canvas>
+  )
 }
 
-export default App;
+export default App
